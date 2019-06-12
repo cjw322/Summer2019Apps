47f1dd4ae1e6fdaf4c89a5b0489514cfb0ff4547
Cora Wu <cjw322@cornell.edu>
2019-06-11 21:27:10 -0700

    Organizational shifts

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Cora Wu on 6/11/19.
+//  Copyright © 2019 Cora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
