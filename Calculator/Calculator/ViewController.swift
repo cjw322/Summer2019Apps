b2aa509dcf7c4ab45241ac560c613a839b21df93
Cora Wu <cjw322@cornell.edu>
2019-06-18 01:21:19 -0700

    Added clear button to design & added background on Xcode project

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,12 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var backgroundImage: UIImageView!
+    var threeButton: UIButton!
+    var circleImage: UIImage!
+    
+    @IBOutlet weak var oneButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .black
+        setBackground()
+        
+        
+        
+        
+        
+//        circleImage = UIImage(named: "circle")
+//
+//        threeButton = UIButton()
+//        threeButton.setBackgroundImage(circleImage, for: .normal)
+//        threeButton.setTitle("3", for: .normal)
+//        threeButton.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(threeButton)
+//
+//        NSLayoutConstraint.activate([
+//            threeButton.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+//            threeButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+//            threeButton.widthAnchor.constraint(equalToConstant: 100),
+//            threeButton.heightAnchor.constraint(equalToConstant: 100)
+//            ])
+        
+        
     }
 
-
+    func setBackground() {
+        
+        let blurFilter = CIFilter(name: "CIGaussianBlur")!
+        blurFilter.setValue(CIImage(image: UIImage(named: "ocean")!), forKey: kCIInputImageKey)
+        blurFilter.setValue(10, forKey: kCIInputRadiusKey)
+        
+        backgroundImage = UIImageView()
+        backgroundImage.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImage.clipsToBounds = false
+        backgroundImage.contentMode = .scaleAspectFill
+        //backgroundImage.image = UIImage(named: "ocean")
+        backgroundImage.image = UIImage(ciImage: blurFilter.outputImage!)
+        backgroundImage.isOpaque = false
+        backgroundImage.alpha = 0.6
+        view.addSubview(backgroundImage)
+        view.sendSubviewToBack(backgroundImage)
+        
+        NSLayoutConstraint.activate([
+            backgroundImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            backgroundImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            backgroundImage.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            backgroundImage.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            ])
+        
+        
+        
+    }
 }
 
